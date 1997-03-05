3d310025e0272d3879badf43e37849fa6b48b264
hkirschk <hkirschk@9a56e421-9c0d-0410-8bba-bec7531eac79>
1997-03-05 15:12:19 +0000

    Initial revision
    git-svn-id: https://svn.code.sf.net/p/plob/code/trunk@89 9a56e421-9c0d-0410-8bba-bec7531eac79

---
+++ plob/tex/diplom/auto/karteic.el
@@ -0,0 +1,13 @@
+(TeX-add-style-hook "karteic"
+ (function
+  (lambda ()
+    (TeX-add-symbols
+     "xSsolid"
+     "mkman"
+     "coliw"
+     "coliiw"
+     "ogrow"
+     "toperson")
+    (TeX-run-style-hooks
+     "diagdefs"))))
+
