f628edd763bba2eb05c1e6fa7ceecd496e783b12
hkirschk <hkirschk@9a56e421-9c0d-0410-8bba-bec7531eac79>
1997-03-06 09:08:21 +0000

    Initial revision
    git-svn-id: https://svn.code.sf.net/p/plob/code/trunk@101 9a56e421-9c0d-0410-8bba-bec7531eac79

---
+++ plob/tex/diplom/auto/bibliogr.el
@@ -0,0 +1,83 @@
+(TeX-add-style-hook "bibliogr"
+ (function
+  (lambda ()
+    (LaTeX-add-bibitems
+     "bib:ah91"
+     "bib:amop91"
+     "bib:at89"
+     "bib:be87"
+     "bib:bj89"
+     "bib:broe89"
+     "bib:br92"
+     "bib:ca88"
+     "bib:cl91"
+     "bib:st90"
+     "bib:co79"
+     "bib:fe89"
+     "bib:fo88"
+     "bib:go89"
+     "bib:gr93"
+     "bib:he89"
+     "bib:it91"
+     "bib:je87"
+     "bib:ka90"
+     "bib:ke89"
+     "bib:ker88"
+     "bib:ko90"
+     "bib:ki89"
+     "bib:ki94a"
+     "bib:ki94b"
+     "bib:ki94b"
+     "bib:kl76"
+     "bib:ma87"
+     "bib:ma92a"
+     "bib:ma92b"
+     "bib:ma92c"
+     "bib:ma93"
+     "bib:mo89a"
+     "bib:mo89b"
+     "bib:mu91"
+     "bib:ni88"
+     "bib:no92"
+     "bib:on94"
+     "bib:pa91a"
+     "bib:pa91b"
+     "bib:pat88"
+     "bib:rh93"
+     "bib:ri89"
+     "bib:ro87"
+     "bib:scha90"
+     "bib:sche92"
+     "bib:schl83"
+     "bib:schm77"
+     "bib:schm80"
+     "bib:schm82"
+     "bib:skw93"
+     "bib:sk87"
+     "bib:sn93"
+     "bib:sn94"
+     "bib:wo93"
+     "bib:sto86a"
+     "bib:sto86b"
+     "bib:sto89"
+     "bib:th90"
+     "bib:uhl93"
+     "bib:vaga89"
+     "bib:wa68"
+     "bib:wa89"
+     "bib:we90"
+     "bib:we93"
+     "bib:wi89"
+     "bib:wi83"
+     "bib:zd90")
+    (TeX-add-symbols
+     '("inrdoop" 1)
+     '("inroods" 1)
+     '("inpea" 1)
+     '("inoocda" 1)
+     '("inpos" 1)
+     '("indtap" 1)
+     '("inaoods" 1)
+     '("manualpagesof" 1)
+     "etal"))))
+
