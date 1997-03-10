0de0a56d7a940119f3acac2154c4355ce7386cb0
hkirschk <hkirschk@9a56e421-9c0d-0410-8bba-bec7531eac79>
1997-03-10 18:26:35 +0000

    Initial revision
    git-svn-id: https://svn.code.sf.net/p/plob/code/trunk@164 9a56e421-9c0d-0410-8bba-bec7531eac79

---
+++ plob/tex/diplom/auto/gctrpe.el
@@ -0,0 +1,13 @@
+(TeX-add-style-hook "gctrpe"
+ (function
+  (lambda ()
+    (TeX-add-symbols
+     "colsep"
+     "rlineh"
+     "etcetc"
+     "mkbox"
+     "hr"
+     "begptr")
+    (TeX-run-style-hooks
+     "diagdefs"))))
+
