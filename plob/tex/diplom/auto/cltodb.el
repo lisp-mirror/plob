633268b0a87c81d389df32e3070160395f520d88
hkirschk <hkirschk@9a56e421-9c0d-0410-8bba-bec7531eac79>
1997-03-10 16:17:43 +0000

    Initial revision
    git-svn-id: https://svn.code.sf.net/p/plob/code/trunk@137 9a56e421-9c0d-0410-8bba-bec7531eac79

---
+++ plob/tex/diplom/auto/cltodb.el
@@ -0,0 +1,19 @@
+(TeX-add-style-hook "cltodb"
+ (function
+  (lambda ()
+    (LaTeX-add-labels
+     "chap:cltodb"
+     "fig:pocache"
+     "sec:ocls"
+     "sec:fnmtd"
+     "sec:extp"
+     "sec:rdbpr"
+     "sec:porepr"
+     "sec:cut"
+     "sec:simpd"
+     "fig:adrpath"
+     "fig:adrname"
+     "fig:adridx"
+     "sec:dbtocl"
+     "fig:trtime"))))
+
