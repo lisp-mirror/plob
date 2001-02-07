053a6cf190282448a400c644d49cfdcd31bbf304
hkirschk <hkirschk@9a56e421-9c0d-0410-8bba-bec7531eac79>
2001-02-07 07:52:17 +0000

    Generator macros for ACL 6.0
    git-svn-id: https://svn.code.sf.net/p/plob/code/trunk@624 9a56e421-9c0d-0410-8bba-bec7531eac79

---
+++ plob/bin/c2allegro6.h
@@ -0,0 +1,50 @@
+/* -------------------------------------------------------------------------
+| Module	c2allegro6.h
+| Author	Heiko Kirschke
+|		mailto:Heiko.Kirschke@acm.org
+| Date		2000-01-06
+| Description	Generator macros for Allegro LISP 6.x.
+|		The rather strange looking 'token' =|| is replaced by '#'
+|		in script c2lisp.
+|
+| Copyright	PLOB! Copyright 1994--2001 Heiko Kirschke.
+|		All rights reserved.
+|
+| Unlimited use, reproduction, modification and distribution of this
+| software is permitted.  Any copy or modified version of this
+| software must include both the above copyright notice of
+| Heiko Kirschke and this paragraph; for each modified version, an
+| additional statement must be added telling the year of modification
+| and quoting the author of the modification.  Any distribution of
+| this software must comply with all applicable German export control
+| laws.  This software is made available AS IS, and HEIKO KIRSCHKE
+| DISCLAIMS ALL WARRANTIES, EXPRESS OR IMPLIED, INCLUDING WITHOUT
+| LIMITATION THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+| A PARTICULAR PURPOSE, AND NOTWITHSTANDING ANY OTHER PROVISION
+| CONTAINED HEREIN, ANY LIABILITY FOR DAMAGES RESULTING FROM THE
+| SOFTWARE OR ITS USE IS EXPRESSLY DISCLAIMED, WHETHER ARISING IN
+| CONTRACT, TORT (INCLUDING NEGLIGENCE) OR STRICT LIABILITY, EVEN IF
+| HEIKO KIRSCHKE IS ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
+|
+| Please note that these license terms adhere only to the code of
+| PLOB!  itself. PLOB! uses POSTORE (Persistent Object Store) as a
+| low-level persistent memory; it is provided in binary form within
+| PLOB! with the permission of the University of St. Andrews
+| (http://www-ppg.dcs.st-andrews.ac.uk/Default.html).  Contact the
+| University of St. Andrews for getting their license terms on
+| POSTORE.
+|
+ ------------------------------------------------------------------------- */
+
+#ifndef C2ALLEGRO6_H
+#define C2ALLEGRO6_H
+
+#include	<c2allegro5.h>
+
+#endif /* C2ALLEGRO6_H */
+
+/*
+  Local variables:
+  buffer-file-coding-system: iso-latin-1-unix
+  End:
+*/
