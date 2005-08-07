e5bdf2bb8d52485938864aef9e32dbfe9390ee8f
hkirschk <hkirschk@9a56e421-9c0d-0410-8bba-bec7531eac79>
2005-08-07 18:38:56 +0000

    Removed empty lines.
    git-svn-id: https://svn.code.sf.net/p/plob/code/trunk@677 9a56e421-9c0d-0410-8bba-bec7531eac79

--- plob/oncrpc-1.12/makebc.bat
+++ plob/oncrpc-1.12/makebc.bat
@@ -1,48 +1,18 @@
 cd librpc
-
-
 nmake -f Makefile.bc %1
-
-
 if errorlevel 1 goto end
-
-
 cd ..\rpcgen
-
-
 nmake -f Makefile.bc %1
-
-
 if errorlevel 1 goto end
-
-
 cd ..\service
-
-
 nmake -f Makefile.bc %1
-
-
 if errorlevel 1 goto end
-
-
 cd ..\rpcinfo
-
-
 nmake -f Makefile.bc %1
-
-
 if errorlevel 1 goto end
-
-
 cd ..\test
-
-
 nmake -f Makefile.bc %1
-
-
 :end
-
-
 cd ..
 
 
